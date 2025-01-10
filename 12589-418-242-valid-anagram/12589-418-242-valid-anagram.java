85585527e8a993920bdfaa606227de0f31d55d90
Robert Bray <112028483+codeMan1777@users.noreply.github.com>
2025-01-09 23:09:04 -0500

    Update 12589-418-242-valid-anagram.java
    updated solution

--- 12589-418-242-valid-anagram/12589-418-242-valid-anagram.java
+++ 12589-418-242-valid-anagram/12589-418-242-valid-anagram.java
@@ -1,26 +1,37 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character,Integer> sHashMap = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            if(!sHashMap.containsKey(s.charAt(i))) {
-                sHashMap.put(s.charAt(i),1);
-            } else {
-                sHashMap.put(s.charAt(i), sHashMap.get(s.charAt(i))+1);
-            }
-        }
-        for (int i = 0; i < t.length(); i++) {
-            if(sHashMap.containsKey(t.charAt(i))) {
-                sHashMap.put(t.charAt(i), sHashMap.get(t.charAt(i))-1);
-            } else {
-                return false;
-            }
-        }
+      HashMap<Character, Integer> sHashMap = new HashMap<>();
 
-        for (Integer val : sHashMap.values()) {
-            if(val!=0) {
-                return false;
-            }
+    
+      //does t have the same letter count as s.
+      //loop through s, and deduct each occurence if its present in t.
+
+      // loop through t and if the value exist in s then decrement it.
+      // make sure that s hashmap has no values other than 0, if it does have only zeros return true.
+      
+
+      for(int i = 0; i < s.length(); i++) {
+        sHashMap.put(s.charAt(i), sHashMap.getOrDefault(s.charAt(i),0) + 1);
+
+      }
+
+      for(int i = 0; i < t.length(); i++) {
+        if(sHashMap.containsKey(t.charAt(i))) {
+            sHashMap.put(t.charAt(i), sHashMap.get(t.charAt(i))  - 1);
+        } else {
+            return false;
         }
-        return true;     
+      }
+
+      for(int i = 0; i < s.length(); i++) {
+        if(sHashMap.get(s.charAt(i)) != 0) {
+            return false;
+        } else {
+            continue;
+        }
+      }
+    
+    return true;
+
     }
 }
